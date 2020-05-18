7ab536b8146f9ff89d181368ddde3534c2c92349
sagarm015 <19ucs182@lnmiit.ac.in>
2020-05-18 18:38:02 +0530

    intial commit with env variables

---
+++ app.js
@@ -0,0 +1,69 @@
+require("dotenv").config();
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const encrypt = require("mongoose-encryption");
+const app=express();
+app.use(express.static("public"));
+app.set("view engine","ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+mongoose.connect("mongodb://localhost:27017/secretDB",{ useNewUrlParser: true , useUnifiedTopology: true });
+
+const userSchema =new mongoose.Schema({
+  email: String,
+  password: String
+});
+
+userSchema.plugin(encrypt,{secret: process.env.SECRET,encryptedFields: ["password"]});
+const User= mongoose.model("User",userSchema);
+
+// REVIEW: gets request for the site
+app.get("/",function(req,res){
+  res.render("home");
+});
+
+app.get("/login",function(req,res){
+  res.render("login");
+});
+
+app.get("/register",function(req,res){
+  res.render("register");
+});
+
+
+
+// REVIEW: posts requests
+
+app.post("/register",function(req,res){
+  const user = new User({
+    email: req.body.username,
+    password: req.body.password
+  });
+  user.save();
+  res.render("secrets");
+});
+
+app.post("/login",function(req,res){
+  User.findOne({email: req.body.username},function(err,result){
+  if(result){
+    if(result.password=== req.body.password){
+      res.render("secrets");
+    }
+    else{
+      res.send("wrong password");
+    }
+
+    }
+    else{
+      res.send("no such username exists");
+    }
+  });
+});
+
+
+
+
+app.listen(3000,function(){
+  console.log(" hello server started at port 3000");
+});
